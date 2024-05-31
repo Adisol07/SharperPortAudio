808be798d6ef4378223f4eea13c92f2c0f4a7b7b
Adisol <62602034+Adisol07@users.noreply.github.com>
2024-05-31 14:40:11 +0200

    Added DemoConsole3

---
+++ DemoConsole3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using SharperPortAudio;
+
+namespace DemoConsole3;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Loading..");
+        AudioRecorder recorder = new AudioRecorder(Device.DefaultInputDevice);
+        recorder.SendBufferInsteadOfChunk = true;
+        recorder.BufferEnabled = true;
+        recorder.DataReceived += (data) =>
+        {
+            Console.WriteLine("Buffer size: " + data.Samples?.Length);
+        };
+        Console.WriteLine("Recording");
+        recorder.Start();
+        Console.ReadKey();
+        recorder.Stop();
+        Console.WriteLine("Stopped");
+    }
+}
